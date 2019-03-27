1f9b83c407248022bc29e131a65cc2f5deed29fe
Thomas Jessen <tbj@eal.dk>
2019-03-27 09:18:16 +0100

    changed to let and added expression example

--- functions/js/tax_expression.js
+++ functions/js/tax_expression.js
@@ -1,4 +1,4 @@
-function calculateTax(subtotal, taxRate){
+let calculateTax =function (subtotal, taxRate){
     let tax = subtotal * taxRate;
     tax = tax.toFixed(2);
     return tax;
@@ -6,6 +6,7 @@ function calculateTax(subtotal, taxRate){
 };
 
 window.onload = function() {
+    console.log("running...");
     let subtotal = 100;
     let taxRate = 0.25;
     let salesTax = calculateTax(subtotal, taxRate);
