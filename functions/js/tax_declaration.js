170b0fa41741a1ef7dce5af58c5d9adc3271e383
Thomas Jessen <tbj@eal.dk>
2019-03-29 13:25:46 +0100

    Update tax_declaration.js

--- functions/js/tax_declaration.js
+++ functions/js/tax_declaration.js
@@ -2,7 +2,7 @@ function calculateTax(subtotal, taxRate){
     let tax = subtotal * taxRate;
     tax = tax.toFixed(2);
     return tax;
-    
+   
 };
 
 window.onload = function() {
@@ -10,7 +10,7 @@ window.onload = function() {
     let taxRate = 0.25;
     let salesTax = calculateTax(subtotal, taxRate);
     
-    document.getElementById('taxOutput').innerHTML ='DK mom"s: ' + salesTax + "<br>";
+    document.getElementById('taxOutput').innerHTML ="DK moms:  + salesTax + "<br>";
     taxRate = 0.20;
     
     salesTax = calculateTax(subtotal, taxRate);
